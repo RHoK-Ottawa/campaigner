931743dbe416be7ddcf254c6fcc47172f6004d2a
Kevin Hughes <kevin.hughes@shopify.com>
2013-12-08 13:08:03 -0500

    cleaned up and added json tests

--- test/functional/campaigns_controller_test.rb
+++ test/functional/campaigns_controller_test.rb
@@ -1,27 +1,45 @@
 require 'test_helper'
 
 class CampaignsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   def setup
     @signatory = signatories(:bob)
     @campaign = campaigns(:epic_petition)
   end
 
-  #test "create" do
-  #  post :create, :campaign_id => @campaign.id ... the rest of the stuff
-  #  assert_response :success
-  #end
+  test "create" do
+    title = "Fairly Epic Petition"
+    petition_text = "This petition is farily epic."
+    post :create, :campaign => {:title => title, :petition_text => petition_text} 
+    assert_response 302
+    assert_equal Campaign.where(:title => title, :petition_text => petition_text).length, 1
+  end
+
+  test "create json" do
+    title = "Fairly Epic Petition"
+    petition_text = "This petition is farily epic."
+    post :create, :format => :json, :campaign => {:title => title, :petition_text => petition_text} 
+    assert_response :success
+    assert_equal Campaign.where(:title => title, :petition_text => petition_text).length, 1
+  end
 
   test "should get index" do
     get :index, :id => @campaign.id 
     assert_response :success
   end
 
+  test "should get index json" do
+    get :index, :format => :json, :id => @campaign.id 
+    assert_response :success
+  end
+
   test "should get show" do
     get :show, :id => @campaign.id, :signatories_id => @signatory.id 
+    assert_response :success
+  end
+
+  test "should get show json" do
+    get :show, :format => :json, :id => @campaign.id, :signatories_id => @signatory.id 
     assert_response :success
   end
 
--- test/functional/signatories_controller_test.rb
+++ test/functional/signatories_controller_test.rb
@@ -8,18 +8,38 @@ class SignatoriesControllerTest < ActionController::TestCase
   end
 
   test "create" do
-    post :create, :campaign_id => @campaign.id, :signatory => {:email => "epic@email.com", :name => "EpiC Name"} 
+    name = "EpiC Name"
+    email = "epic@email.com"
+    post :create, :campaign_id => @campaign.id, :signatory => {:email => email, :name => name} 
     assert_response 302
-    assert_equal @campaign.signatories.where(:email => "epic@email.com", :name => "EpiC Name").length, 1
+    assert_equal @campaign.signatories.where(:email => email, :name => name).length, 1
+  end
+
+  test "create json" do
+    name = "EpiC Name"
+    email = "epic@email.com"
+    post :create, :format => :json, :campaign_id => @campaign.id, :signatory => {:email => email, :name => name} 
+    assert_response :success
+    assert_equal @campaign.signatories.where(:email => email, :name => name).length, 1
   end
 
   test "should get index" do
     get :index, :campaign_id => @campaign.id 
     assert_response :success
   end
+
+  test "should get index json" do
+    get :index, :format => :json, :campaign_id => @campaign.id 
+    assert_response :success
+  end
   
   test "should get show" do
     get :show, :campaign_id => @campaign.id, :id => @signatory.id 
+    assert_response :success
+  end
+
+  test "should get show json" do
+    get :show, :format => :json, :campaign_id => @campaign.id, :id => @signatory.id 
     assert_response :success
   end
 
