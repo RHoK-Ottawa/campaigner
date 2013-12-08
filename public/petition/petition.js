9b2a7a1d3625322964a5333f0d9544de4a0566da
Anton McConville <anton@hickory.ca>
2013-12-07 19:38:24 -0500

    More implementation of petition page - button, fields

---
+++ public/petition/petition.js
@@ -0,0 +1,8 @@
+function filePetition(){
+
+	var signatory = document.getElementById( 'signatory' );
+	var email = document.getElementById( 'emailaddress' );
+	
+	console.log( 'signatory: ' + signatory.value );
+	console.log( 'emailaddress: ' + emailaddress.value );
+}
