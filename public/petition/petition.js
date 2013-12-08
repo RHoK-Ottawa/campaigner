33f8390b10de8d3726ed9e0626328908a976330f
Anton McConville <anton@hickory.ca>
2013-12-07 19:47:05 -0500

    XHR push of signature

--- public/petition/petition.js
+++ public/petition/petition.js
@@ -1,10 +1,22 @@
 function filePetition(){
 
-	var signatory = document.getElementById( 'signatory' );
+	var signatoryName = document.getElementById( 'signatory' );
 	var email = document.getElementById( 'emailaddress' );
 	
 	console.log( 'signatory: ' + signatory.value );
 	console.log( 'emailaddress: ' + emailaddress.value );
+	
+	var signature = { "email": signatory.value, "name": signatoryName };
+	
+	var xhr = new XMLHttpRequest();
+	
+	xhr.open('POST', 'http://localhost:3000/campaigns/4/signatories/new', true);
+	
+	xhr.onload = function () {
+	    console.log('onload');
+	};
+	
+	xhr.send( signature );
 
   // needs to hit:
   // http://localhost:3000/campaigns/4/signatories/new
