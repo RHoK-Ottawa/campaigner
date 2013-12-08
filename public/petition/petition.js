f5484d964f9a40492454b3179348ec1b0edc6161
Kevin Hughes <kevin.hughes@shopify.com>
2013-12-07 19:41:40 -0500

    added post details to petition app

--- public/petition/petition.js
+++ public/petition/petition.js
@@ -5,4 +5,12 @@ function filePetition(){
 	
 	console.log( 'signatory: ' + signatory.value );
 	console.log( 'emailaddress: ' + emailaddress.value );
+
+  // needs to hit:
+  // http://localhost:3000/campaigns/4/signatories/new
+    // json:
+    //{
+    //"email": "joeyB@jsoey.com",
+    //"name": "joe"
+    //}
 }
