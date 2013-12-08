8717ebd5c2a92a659a3bc557f3da0d3f7557a189
Kevin Hughes <kevin.hughes@shopify.com>
2013-12-07 19:30:20 -0500

    added the petition app to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Petitions::Application.routes.draw do
+  
+  match 'app', :to => redirect('/petition/petition'), via: [:get]
+
   resources :campaigns do
     resources :signatories do
       get 'success', on: :member
